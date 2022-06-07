7a01231ae5acaad8d56834c7fe377c4b352510d1
Putu Widyantara <putuwaw973@gmail.com>
2022-06-07 22:56:27 +0800

    Create information page

---
+++ pages/informasi.js
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+export default function Informasi() {
+    return (
+        <>
+        <Navbar/>
+            <div className="container p-6 max-w-md mx-auto bg-cyan-100 sm:max-w-xl md:max-w-2xl lg:max-w-5xl min-h-screen">
+                <h1 className="font-bold text-3xl">Informasi</h1>
+                <p>Ini adalah informasi mengenai aplikasi.</p>
+            </div>
+        <Footer/>
+        </>
+    );
+}
