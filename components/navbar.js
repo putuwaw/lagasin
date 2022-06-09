5612c9014bbb35e825416d00e62a4c7ea8431ebe
Putu Widyantara <putuwaw973@gmail.com>
2022-06-09 20:19:07 +0800

    Change navbar position

--- components/navbar.js
+++ components/navbar.js
@@ -4,16 +4,16 @@ export default function Navbar() {
 	const [show, setShow] = useState(false);
 	return(
 		<>
-			<nav>
-				<div className="bg-blue-500">
-					<div className="flex flex-col md:flex-row md:justify-between  max-w-7xl mx-auto">
+			<nav className="bg-blue-500 fixed z-50 w-full">
+				<div className="md:max-w-5xl md:mx-auto">
+					<div className="flex flex-col md:flex-row md:justify-between max-w-7xl mx-auto">
 						<div className="flex items-center justify-between border-b p-4 md:border-b-0 md:py-0">
 							<div>
 								<a href="#" className="font-semibold uppercase text-white">
 									Lagasin
 								</a>
 							</div>
-							<div>
+							<div className="py-1">
 								<button onClick={()=>setShow(!show)} className="block text-white focus:outline-none md:hidden">
 									<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} >
 										<path className={!show ? 'block' : 'hidden'} strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
@@ -31,7 +31,7 @@ export default function Navbar() {
 								</Link>
 								<Link href="/aplikasi">
 									<a className="aplikasi block px-4 py-2 text-cyan-100 hover:text-white md:py-5">
-											Aplikasi
+										Aplikasi
 									</a>
 								</Link>
 								<Link href="/informasi">
