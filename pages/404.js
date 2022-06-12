f859678271d90ca1c301e4d9b37e4d22ee0d64f1
Putu Widyantara <putuwaw973@gmail.com>
2022-06-12 23:36:24 +0800

    Create rough sketch for 404 error page

--- pages/404.js
+++ pages/404.js
@@ -2,11 +2,14 @@ import Link from 'next/link'
 
 export default function Costom404() {
   return <>
-    <h1>404 - Page Not Found</h1>
+  <div className='bg-white min-h-screen text-center flex flex-col justify-center items-center'>
+    <h1 className='text-blue-600 font-bold text-6xl'>404</h1>
+    <h3 className='text-blue-500 font-semibold mt-3'>Maaf, website yang anda cari tidak ditemukan.</h3>
     <Link href="/">
-      <a className='text-blue-500'>
-        Kembali ke halaman utama.
+      <a className='bg-blue-500 uppercase text-white w-fit p-2 rounded-md mt-3 hover:bg-blue-600 shadow-xl'>
+        Kembali ke beranda
       </a>
     </Link>
+  </div>
   </>
 }
