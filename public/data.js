b58866927210c0bed68d0471b8633c81048e7c47
Putu Widyantara <putuwaw973@gmail.com>
2022-06-12 23:40:03 +0800

    Create data for markers

---
+++ public/data.js
@@ -0,0 +1,71 @@
+var dataJson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          114.93662059307098,
+          -8.192954706185185
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          114.93804216384886,
+          -8.192259139134764
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          114.95200574398041,
+          -8.191621977411053
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          114.88756164908409,
+          -8.20260092765849
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          114.9422639608383,
+          -8.223348735617037
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          114.94332343339921,
+          -8.195856428893714
+        ]
+      }
+    }
+  ]
+}
