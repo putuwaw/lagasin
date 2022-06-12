c8315f9fffab266de399a828e2d54079df50ec26
Putu Widyantara <putuwaw973@gmail.com>
2022-06-12 23:39:28 +0800

    Create document and move global style

---
+++ pages/_document.js
@@ -0,0 +1,12 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+export default function Document() {
+  return (
+    <Html lang='en' className='scroll-smooth box-border'>
+      <Head />
+      <body className='bg-white'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
