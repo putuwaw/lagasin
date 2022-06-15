6b67c9d085c5a70aad06aae9f0ef35296c669a21
Putu Widyantara <putuwaw973@gmail.com>
2022-06-15 23:00:46 +0800

    Create final design for navbar and footer

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,7 @@
-import Image from "next/image"
-export default function Footer(){
-    return (
-        <footer className="bg-blue-800 flex w-full items-center justify-center text-white text-base h-9 ">
-            Copyright &copy; Lagasin 2022
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className="bg-blue-500 flex w-full items-center justify-center text-white text-md h-9 shadow-lg ">
+      Copyright &copy; Lagasin 2022
+    </footer>
+  );
 }
